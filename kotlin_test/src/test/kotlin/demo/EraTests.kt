86fdd952d0858d46ab60b9a45de8e951ab42e102
nyasba <nyasba@gmail.com>
2019-02-04 10:19:09 +0900

    説明用にテストを調整

--- kotlin_test/src/test/kotlin/demo/EraTests.kt
+++ kotlin_test/src/test/kotlin/demo/EraTests.kt
@@ -7,9 +7,7 @@ import io.kotlintest.tables.row
 import java.time.LocalDate
 
 class EraTests : StringSpec({
-
     forall(
-            row(LocalDate.of(1989, 1, 7), Era.昭和),
             row(LocalDate.of(1989, 1, 8), Era.平成),
             row(LocalDate.of(2019, 4, 30), Era.平成),
             row(LocalDate.of(2019, 5, 1), Era.新元号)
@@ -18,5 +16,4 @@ class EraTests : StringSpec({
             Era.from(date) shouldBe expected
         }
     }
-
 })
