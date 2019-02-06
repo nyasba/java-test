00d7d96fc29004d9125d37a1de922af9f040505c
nyasba <nyasba@gmail.com>
2019-02-07 01:03:22 +0900

    JUnit5のMethodSourceのParameterizedTestを追加

--- java_junit5/src/test/java/tddbc/Era2Test.java
+++ java_junit5/src/test/java/tddbc/Era2Test.java
@@ -2,10 +2,13 @@ package tddbc;
 
 import java.time.DateTimeException;
 import java.time.LocalDate;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("元号のテスト")
@@ -32,4 +35,20 @@ public class Era2Test {
         Assertions.assertEquals(Era.新元号, Era.from(date));
     }
 
+
+    @ParameterizedTest(name = "[{index}] {0}は{1}")
+    @MethodSource("eraArguments")
+    void test(LocalDate date, Era expected){
+        Assertions.assertEquals(expected, Era.from(date));
+    }
+
+    private static Stream<Arguments> eraArguments(){
+        return Stream.of(
+            Arguments.of(LocalDate.of(1989, 1, 7), Era.昭和),
+            Arguments.of(LocalDate.of(1989, 1, 8), Era.平成),
+            Arguments.of(LocalDate.of(2019, 4, 30), Era.平成),
+            Arguments.of(LocalDate.of(2019, 5, 1), Era.平成),
+            Arguments.of(LocalDate.of(2019, 5, 1), Era.新元号)
+        );
+    }
 }
